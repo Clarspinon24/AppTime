7b0a27562e904448b269cdb3c31f561b37272271
Clara Marchal <marchal.clara95@gmail.com>
2026-04-10 15:20:02 +0200

    update PWA

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'apptime-v1';
+// Liste des fichiers à mettre en cache
+const ASSETS = [
+  './',
+  './index.html',
+  './icons/logo.png' // Assure-toi que le chemin est correct
+];
+
+// Installation : Mise en cache des ressources
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activation : Nettoyage des anciens caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Stratégie "Cache First" : On pioche dans le cache, sinon on va sur le réseau
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
