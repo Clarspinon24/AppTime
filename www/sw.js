509c9f4b3359b5f1682d0274a6a4645326c7256b
Clara <187749078+Clarspinon24@users.noreply.github.com>
2026-04-16 00:01:08 +0200

    fix: fusion file android

--- www/sw.js
+++ www/sw.js
@@ -1,38 +1,38 @@
-const CACHE_NAME = 'apptime-v1';
+// const CACHE_NAME = 'apptime-v1';
 
-const ASSETS = [
-  './',
-  './index.html',
-  './girlmath.png',
-  './icons/logo.png',
-  './icons/logo2.png'
-];
+// const ASSETS = [
+//   './',
+//   './index.html',
+//   './girlmath.png',
+//   './icons/logo.png',
+//   './icons/logo2.png'
+// ];
 
-// Installation : Mise en cache des ressources
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
-  );
-});
+// // Installation : Mise en cache des ressources
+// self.addEventListener('install', (event) => {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME)
+//       .then((cache) => cache.addAll(ASSETS))
+//       .then(() => self.skipWaiting())
+//   );
+// });
 
-// Activation : Nettoyage des anciens caches
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
-  );
-});
+// // Activation : Nettoyage des anciens caches
+// self.addEventListener('activate', (event) => {
+//   event.waitUntil(
+//     caches.keys().then((keys) => {
+//       return Promise.all(
+//         keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+//       );
+//     })
+//   );
+// });
 
-// Stratégie "Cache First" : On pioche dans le cache, sinon on va sur le réseau
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+// // Stratégie "Cache First" : On pioche dans le cache, sinon on va sur le réseau
+// self.addEventListener('fetch', (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then((response) => {
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
